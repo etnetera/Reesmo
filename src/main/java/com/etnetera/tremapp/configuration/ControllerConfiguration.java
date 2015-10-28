fb6006ddc531801adc3bf8fbc172df96c3fe6734
ZKa <zdenek.kalina@etnetera.cz>
2015-10-28 14:23:36 +0100

    log bad request

---
+++ src/main/java/com/etnetera/tremapp/configuration/ControllerConfiguration.java
@@ -0,0 +1,23 @@
+package com.etnetera.tremapp.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ControllerConfiguration {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ControllerConfiguration.class);
+	
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public void handle(HttpMessageNotReadableException e) {
+        LOGGER.warn("Returning HTTP 400 Bad Request", e);
+        throw e;
+    }
+	
+}
